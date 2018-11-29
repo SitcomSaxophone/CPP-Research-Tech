ceb5b02c8df916204c6d6dca9a083a1e5dcbd591
Samuel Solberg <pauliesays65@gmail.com>
2018-11-29 13:40:28 -0600

    added comments to hello.cpp and input.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <iostream> // directive telling the file to use the standard cout, cin, and endl words from the iostream file
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "Hello, World!" << std::endl; // output the text, "Hello, world!" when the compiled file is run in the terminal
+    return 0; // must return something to finish the execution
 }
--- input.cpp
+++ input.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include <iostream> // directive telling the file to use the standard cout, cin, and endl words from the iostream file
 
 int main()
 {
-    std::cout << "Enter a number: ";
-    int x;
-    std::cin >> x;
-    std::cout << "You entered " << x << std::endl;
-    return 0;
+    std::cout << "Enter a number: "; // ouput text to the user to have them enter a number
+    int x; // initialize variable "x". This does not need a default value as it is going to change anyway
+    std::cin >> x; // define "x" as whatever number is input by the user
+    std::cout << "You entered " << x << std::endl; // output text letting the user know what number it is they input for "x"
+    return 0; // must return something to finish the execution
 }
